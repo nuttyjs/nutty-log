968d9b4b7bfb49c3e6d01023046fef8f445536f9
jmjuanes <josemijuanes@gmail.com>
2017-01-26 11:32:29 +0100

    test.js: init test

---
+++ test.js
@@ -0,0 +1,20 @@
+//Import log
+var log = require('./index.js');
+
+//Print a fatal message
+log.fatal('My fatal message');
+
+//Print an error message
+log.error('My error message');
+
+//Print a warning message
+log.warning('My warning message');
+
+//Print a notice message
+log.notice('My notice message');
+
+//Print an info message
+log.info('My info message');
+
+//Print a debug message
+log.debug('My debug message');
