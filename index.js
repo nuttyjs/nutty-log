acf2599eac21e06e1c2fcac33b0811a4e8ef79ac
jmjuanes <josemijuanes@gmail.com>
2017-02-09 13:40:54 +0100

    index.js: parse a message. Print a json object in message

--- index.js
+++ index.js
@@ -61,8 +61,28 @@ Object.keys(log.levels).forEach(function(level)
   //Add the log level
   log[level] = function(text)
   {
-    //Get the message
-    var message = log.message(level, text);
+    //Initialize the message
+    var message = '';
+
+    //Check for object
+    if(typeof text === 'object')
+    {
+      //Read the object
+      for(var key in text)
+      {
+        //Get the value
+        //var value = JSON.stringify(text[key]);
+        var value = text[key];
+
+        //Build the message
+        message = message + log.message(level, key + ' : ' + value);
+      }
+    }
+    else
+    {
+      //Get the simple message
+      message = log.message(level, text);
+    }
 
     //Check the log level
     if(log.levels[level].index >= log.levels[log._level].index)
@@ -70,14 +90,24 @@ Object.keys(log.levels).forEach(function(level)
       //Get the level color
       var color = log.levels[level].color;
 
-      //Get the index
-      var index = message.indexOf('] [');
+      //Get the stream
+      var stream = (log.levels[level].print === 'error') ? process.stderr : process.stdout;
 
-      //Get the message with colors
-      var msg = colors.gray(message.substring(0, index + 1)) + colors[color](message.substring(index + 1));
+      //Split the message
+      message.split('\n').forEach(function(msg)
+      {
+        //Check for empty message
+        if(msg.trim() === ''){ return true; }
 
-      //Check for display on stderr or stdout
-      (log.levels[level].print === 'error') ? process.stderr.write(msg) : process.stdout.write(msg);
+        //Get the index
+        var index = msg.indexOf('] [');
+
+        //Get the message with colors
+        msg = colors.gray(msg.substring(0, index + 1)) + colors[color](msg.substring(index + 1)) + '\n';
+
+        //Print the message
+        stream.write(msg);
+      });
     }
 
     //Return the message
@@ -85,36 +115,61 @@ Object.keys(log.levels).forEach(function(level)
   };
 });
 
-//Print a json in console
-log.json = function(level, obj)
+//Parse a list of messages
+log.parse = function(messages)
 {
-  //Get the object keys
-  var keys = (Array.isArray(obj) === false) ? Object.keys(obj) : Array.apply(null, { length: obj.length }).map(Number.call, Number);
-
-  //Output messages
-  var out = [];
-
-  //Read all the keys
-  for(var i = 0; i < keys.length; i++)
+  //Check for string
+  if(typeof messages === 'string')
   {
-    //Get the key
-    var key = keys[i];
-
-    //Get the value
-    var value = obj[key];
-
-    //Check the value
-    if(typeof value === 'object'){ value = JSON.stringify(value); }
-
-    //Print the message in console and return
-    var message = log[level](key + ' : ' + value);
-
-    //Save the message
-    out.push(message);
+    //Contert into array
+    messages = messages.replace(/\r/g, '\n').replace(/\n\n/g, '\n').split('\n');
   }
 
-  //Return the messages list
-  return out.join('');
+  //Initialize the output list
+  var list = [];
+
+  //Read all the messages
+  messages.forEach(function(el)
+  {
+    //Initialize the output object
+    var obj = {};
+
+    //Check the first character
+    if(el.charAt(0) !== '['){ return true; }
+
+    //Get the position of the ] [
+    var index1 = el.indexOf('] [');
+
+    //Check the index value
+    if(index1 === -1){ return true; }
+
+    //Get the time
+    obj.time = el.substring(1, index1);
+
+    //Check the length
+    if(obj.time.length !== 19){ return true; }
+
+    //Get the next ]
+    var index2 = el.indexOf(']', index1 + 1);
+
+    //Check for no index
+    if(index2 === -1){ return true; }
+
+    //Get the level
+    obj.level = el.substring(index1 + 3, index2).toLowerCase();
+
+    //Check the level value
+    if(typeof log.levels[obj.level] === 'undefined'){ return true; }
+
+    //Save the message
+    obj.message = el.substring(index2 + 1).trim();
+
+    //Add to the list
+    list.push(obj);
+  });
+
+  //Return the list of messages
+  return list;
 };
 
 //Exports to node
